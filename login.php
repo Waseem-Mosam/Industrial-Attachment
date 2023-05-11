32cbf08320ea8d9369b7f959b1bcdb26c0e71897
Waseem-Mosam <waseem.mosam1@gmail.com>
2023-05-11 12:51:53 +0200

    comments login.php

--- login.php
+++ login.php
@@ -1,9 +1,10 @@
 <?php 
 session_start(); 
+//establish database connection
 include "assets/dbconnect.php";
-
+//Checks if data has been entered in form fields
 if (isset($_POST['uname']) && isset($_POST['password'])) {
-
+	//function used to sanitize entered data
 	function validate($data){
        $data = trim($data);
 	   $data = stripslashes($data);
@@ -13,7 +14,7 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
 
 	$uname = validate($_POST['uname']);
 	$pass = validate($_POST['password']);
-
+	//form validation
 	if (empty($uname)) {
 		header("Location: index.php?error=User Name is required");
 	    exit();
@@ -21,11 +22,12 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
         header("Location: index.php?error=Password is required");
 	    exit();
 	}else{
+		//password encryption
 		$pass = md5($pass);
 		$sql = "SELECT * FROM iams_user WHERE email='$uname' AND password='$pass'";
 
 		$result = mysqli_query($conn, $sql);
-
+		//validate login details to allow user to access system
 		if (mysqli_num_rows($result) === 1) {
 			$row = mysqli_fetch_assoc($result);
             if ($row['email'] === $uname && $row['password'] === $pass) {
@@ -33,6 +35,7 @@ if (isset($_POST['uname']) && isset($_POST['password'])) {
             	$_SESSION['name'] = $row['firstName'];
             	$_SESSION['id'] = $row['id'];
                 $_SESSION['role'] = $row['role'];
+		//redirection to relevant page depending on role
                 if($_SESSION['role']=="Student"){
                     header("Location: dashboards/studentDash.php");
 		            exit();
