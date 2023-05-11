8da3fb71fe786a48a433e8d13c040869d90531f6
Waseem-Mosam <waseem.mosam1@gmail.com>
2023-05-11 13:10:10 +0200

    comments uploadLog.php

--- dashboards/functions/uploadLog.php
+++ dashboards/functions/uploadLog.php
@@ -1,4 +1,5 @@
 <?php 
+//establish database connection and check if necessary data has been entered on form
 include "../../assets/dbconnect.php";
 
 if (isset($_POST['ID']) && isset($_POST['date']) && !empty($_FILES['pdfFile']['name'])) {
@@ -10,24 +11,24 @@ if (isset($_POST['ID']) && isset($_POST['date']) && !empty($_FILES['pdfFile']['n
 	   return $data;
 	}
 
-	
+	//validate student login with sanitize function
 	$studID = validate($_POST['ID']);
 
     $date = $_POST['date'];
     
     $file_name = $_FILES['pdfFile']['name'];
     $file_tmp = $_FILES['pdfFile']['tmp_name'];
-
+//converts pdf to binary format 
     $pdf_blob = fopen($file_tmp, "rb");
     
-
+	//ensures that student id has been entered
 	if(empty($studID)){
         header("Location: ../studentDash.php?error=Student ID is required");
 	    exit();
 	}else{
-        
+        	//insert data into table
 		$sql = "INSERT INTO iams_logbook (name, date, file, id) VALUES ('".$file_name."', '". $date ."', '".$pdf_blob."',  '".$studID."');";
-    
+    		//checks if insertion was successful or not 
 		if ($conn->query($sql) === TRUE) {
             header("Location: ../studentDash.php");
         } else {
