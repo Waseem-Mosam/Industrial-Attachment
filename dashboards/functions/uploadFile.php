baf397200a4aedee588664b1405c54adf3195048
Waseem-Mosam <waseem.mosam1@gmail.com>
2023-05-11 13:12:21 +0200

    comments uploadFile.php

--- dashboards/functions/uploadFile.php
+++ dashboards/functions/uploadFile.php
@@ -1,4 +1,5 @@
 <?php 
+//establishes database connection and checks if data has been entered on form 
 include "../../assets/dbconnect.php";
 
 if (isset($_POST['studID']) && !empty($_FILES['pdf_file']['name'])) {
@@ -10,22 +11,22 @@ if (isset($_POST['studID']) && !empty($_FILES['pdf_file']['name'])) {
 	   return $data;
 	}
 
-	
+	// validate student id with sanitize function
 	$studID = validate($_POST['studID']);
     
     $file_name = $_FILES['pdf_file']['name'];
     $file_tmp = $_FILES['pdf_file']['tmp_name'];
-
+//converts pdf to binary format
     $pdf_blob = fopen($file_tmp, "rb");
     
-
+	//checks if student id has been entered
 	if(empty($studID)){
         header("Location: ../studentDash.php?error=Student ID is required");
 	    exit();
 	}else{
-        
+        	// insert data into database
 		$sql = "INSERT INTO iams_report (id, name, file) VALUES ('".$studID."', '".$file_name."', '".$pdf_blob."');";
-    
+    		//checks if insertion into table was successful
 		if ($conn->query($sql) === TRUE) {
             header("Location: ../studentDash.php");
         } else {
