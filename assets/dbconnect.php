a4438de8ffe22860643539237394289673b53437
Waseem-Mosam <waseem.mosam1@gmail.com>
2023-05-11 12:53:58 +0200

    comments dbconnect.php

--- assets/dbconnect.php
+++ assets/dbconnect.php
@@ -1,11 +1,12 @@
 <?php
-
+//database details including username, password, host address and database name
 $sname= "localhost";
 $unmae= "mor01442";
 $password = "mor01442";
 
 $db_name = "iams";
 
+//OOP database connection using mysqli
 $conn = mysqli_connect($sname, $unmae, $password, $db_name);
 
 if (!$conn) {
